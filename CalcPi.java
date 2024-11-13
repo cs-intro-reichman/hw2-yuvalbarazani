90974c5fd6f1777046a21773a09acebf6e981d2f
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-11-13 02:36:12 +0200

    fiinished

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,12 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num=Integer.parseInt(args[0]);
+		double sum=0;
+		for(int i=1;i<=num;i++)
+		{
+			sum+= Math.pow(-1.0,i+1)*(1.0/(2*i-1));
+		}
+		System.out.println(4*sum);
 	}
 }
